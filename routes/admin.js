de54c93f6b86ae953e6c980cec7c057f36dc6e3e
Alejo calandra <alejocalandra18@gmail.com>
2023-03-08 19:13:43 -0300

    agregamos propiedades al admin

--- routes/admin.js
+++ routes/admin.js
@@ -4,13 +4,18 @@ const {
 	crearProducto,
 	cargarProductos,
 	cargarUsuarios,
+	eliminarProducto,
+	editarProducto,
 } = require('../controllers/admin');
+const { validarJWT } = require('../middlewares/validar-jwt');
 const { validarCampos } = require('../middlewares/validarCampos');
 const routerAdmin = express.Router();
 
 routerAdmin.post(
 	'/new',
+
 	[
+		validarJWT,
 		check('name', 'el nombre es obligatorio').not().isEmpty(),
 		check('precio', 'el precio es obligatorio').not().isEmpty(),
 		check('cantidad', 'la cantidad es obligatorio').not().isEmpty(),
@@ -21,8 +26,12 @@ routerAdmin.post(
 	crearProducto
 );
 
-routerAdmin.get('/productos', cargarProductos);
+routerAdmin.get('/productos', validarJWT, cargarProductos);
 
-routerAdmin.get('/usuarios', cargarUsuarios);
+routerAdmin.get('/usuarios', validarJWT, cargarUsuarios);
+
+routerAdmin.delete('/eliminar/:id', validarJWT, eliminarProducto);
+
+routerAdmin.put('/editar', validarJWT, editarProducto);
 
 module.exports = routerAdmin;
